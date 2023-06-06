7b7a594432b9307453b4dfaa89e4e219e61127e0
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2023-06-06 11:28:00 -0500

    Use flexTheme

--- lib/ui/my_app.dart
+++ lib/ui/my_app.dart
@@ -1,6 +1,8 @@
 import 'package:f_navigation/ui/pages/page1.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'pages/page2.dart';
 import 'pages/page3a.dart';
@@ -13,10 +15,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      debugShowCheckedModeBanner: false,
+      theme: FlexThemeData.light(
+        scheme: FlexScheme.greenM3,
+        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+        blendLevel: 7,
+        subThemesData: const FlexSubThemesData(
+          blendOnLevel: 10,
+          blendOnColors: false,
+          useM2StyleDividerInM3: true,
+        ),
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
         useMaterial3: true,
+        swapLegacyOnMaterial3: true,
+        fontFamily: GoogleFonts.notoSans().fontFamily,
       ),
+      themeMode: ThemeMode.light,
       initialRoute: '/page1',
       getPages: [
         GetPage(name: '/page1', page: () => const Page1()),
